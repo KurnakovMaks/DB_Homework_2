53bab295cbac788279f38edd7d7d24b41f302382
Max <maks.kurnakov@mail.ru>
2020-10-24 17:46:29 +0300

    add APIs from 6th seminar

--- seminars/seminar5/index.js
+++ seminars/seminar5/index.js
@@ -2,7 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const { idleCount } = require('./config/db')
 const pool = require('./config/db')
+    // body parser, чтобы была возможность парсить body
+const bodyParser = require('body-parser')
+
 const app = express()
+//чтобы парсить json
+app.use(bodyParser.json())
+
+
+//const app = express()
 
 app.route('/now').get(async(reg,res)=>{
     const pgclient = await pool.connect()
@@ -39,43 +47,137 @@ catch(err){
         console.log('close db connection')
     }})
 
+    //сделать новый заказ
     app.route('/make_order/:id').post(async(req,res) => {
         //todo: получить айди не из парам а из токена
-        let pgclient
+
+        //todo: обраб ошибку если не удалось подключиться
+        let pgclient = await pool.connect()
         try{
-            pgclient = await pool.connect()
+            
             const {id} = req.params
+
+            //открытие транзакции
+            await pgclient.query('BEGIN')
+
+            //создали заказ и получили его айди
             const {rows } = await pgclient.query(`
             INSERT INTO order_ (client_id) VALUES ($1) RETURNING id
             `, [id])
     const orderID = rows[0].id
-    res.send({
-        order_id: orderID
-    })
 
-//todo: 1)определ со структурой, кот будем передавать
-// мб так:
-// [
-//     {
-//         menu_id:1,
-//         count:2
-//     }
-// // ]
-// 2) все делать в транзации
-//3) определ как считать стоимость заказа
-// 4) добавить все позиции заказа (все продукты из заказа) в ордер меню
 
+    // параметры для подготовки запроса
+    let params = []
+    let values = []
+    for (const [i,item] of req.body.entries())
+    {
+        params.push(`$${i+1}`)
+        values.push(item.menu_id)
     }
+
+    //получить стоимость из меню
+    const {rows: costQueryRes} = await pgclient.query(`
+    Select id,price::numeric
+    from menu
+    where id IN(${params.join(',')})
+    `, values)
+
+
+
+    //созд нов перем кот включ то же что и входной боди
+    //но с вычисленной ценой
+    let orderWithCost = []
+    //для этого пройдем по каждому эл-ту боди
+    for(const item of req.body)
+    {
+        //и для кажд эл-та найти цену в костквери
+        //полученном при помощи запроса
+        let cost = null
+        for (const costItem of costQueryRes)
+        {
+            //ищем совпадения айди в костквери с меню айди переданном в боди
+            if (costItem.id === item.menu_id)
+            {
+                cost = costItem.price
+            }
+        }
+
+        //тут кост либо нал либо с значением цены
+        //если кост нал, означ что такого товара в меню не найдено //ошибка
+        //делаем ролбэк и вернуть сообщ клиенту
+        if (!cost)
+        {
+            throw new Error(`not found in menu: ${item.menu_id}`)
+        }
+
+        orderWithCost.push({
+            ... item,
+            cost: cost * item.count
+        })
+    }
+
+    let promises = []
+    for (const item of orderWithCost)
+    {
+        promises.push(pgclient.query(`
+        INSERT INTO order_menu (order_id, menu_id, count, price)
+        VALUES ($1, $2, $3, $4);
+        `, [orderID, item.menu_id, item.count, item.cost]))
+    }
+
+    await Promise.all(promises)
+
+    await pgclient.query('COMMIT')
+
+    res.send({ order_id: orderID })
+}
     
-    catch(err){
+    catch(err)
+    {
+        await pgclient.query('ROLLBACK')
         res.status(500).send(
             { error: err.message })
             console.error(err)
     }
     
-    finally{
+    finally
+    {
         await pgclient.release()
         console.log('close db connection')
+    }
+})
+
+
+//регистрация
+//в postman { "error": "null value in column \"name\" violates not-null constraint" }
+app.route('/sign_up').post(async (req,res) => 
+{
+    const { name, address, phone, email, password } = req.body
+    
+    let pgclient = await pool.connect()
+    
+    try
+    {
+        const { rows } = await pgclient.query(`
+        INSERT INTO client (name, address, phone, email, password)
+        VALUES ($1,$2,$3,$4,$5) RETURNING id;
+        `, [name, address, phone, email, password])
+        
+        res.send({ id: rows[0].id })
+    }
+        
+    catch(err)
+    {
+        res.status(500).send(
+            {
+                error: err.message
+            })
+            console.error(err)
+    }
+    finally
+    {
+        await pgclient.release()
     }
 })
 
---
+++ seminars/seminar5/migrations/1_add_email_password_up.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+    ALTER TABLE client
+	ADD COLUMN email varchar
+    (255);
+
+    ALTER TABLE client
+	ADD COLUMN password varchar
+    (255);
+
+    COMMIT; 
