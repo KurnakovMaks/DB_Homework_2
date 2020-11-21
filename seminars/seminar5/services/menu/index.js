f92d009ef663320429205679b484054166624e77
Max <maks.kurnakov@mail.ru>
2020-11-21 12:43:43 +0300

    req with param

--- seminars/seminar5/services/menu/index.js
+++ seminars/seminar5/services/menu/index.js
@@ -1,20 +1,24 @@
 const pool = require('../../config/db')
 
 //возвращает всё меню или выполняет поиск по названию
-async function findMenu(name) {
-
-    let countPos = 1;
+async function findMenu(name, price) {
     let query = `
     Select * from menu
     where 1=1`
     const values = []
 
+    let countPos = 1
     if (name) {
-        query += 'and name ilike $countPos'
-        values.push(`%${name}%`),
-        countPos++; //решение $1
+        query += `and name ilike $${countPos}`
+        values.push(`%${name}%`), countPos++ //решение $1
     }
     console.log(query)
+
+    if (price) {
+        query += ` AND price < $${countPos}`
+        values.push(price)
+        countPos++
+    }
 
     const { rows } = await pool.query(query, values)
     return rows
