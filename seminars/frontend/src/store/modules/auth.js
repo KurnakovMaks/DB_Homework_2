2677bf41259cfdf0585c7f84606427fab73bf145
Max <maks.kurnakov@mail.ru>
2020-11-21 12:49:04 +0300

    add check_auth

--- seminars/frontend/src/store/modules/auth.js
+++ seminars/frontend/src/store/modules/auth.js
@@ -12,6 +12,12 @@ const getters = {}
 
 // actions
 const actions = {
+  setAuth({ commit }, token) {
+    this._vm.$axios.defaults.headers.common['Authorization'] = token
+    const user = jwtDecode(token)
+    commit('setUser', user)
+  },
+
   async signIn({ commit }, credentials) {
     //запрос к sign_up Api
     const resp = await this._vm.$axios.post('/sign_in', credentials)
@@ -19,9 +25,16 @@ const actions = {
     // добавляем к axios header по умолчанию
     // т.е. чтобы все запросы к бэку отправлялись с токеном
     this._vm.$axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    localStorage.setItem('token', `Beerer ${token}`)
 
     const user = jwtDecode(token)
     commit('setUser', user)
+  },
+
+  logout({ commit }) {
+    delete this._vm.$axios.defaults.headers.common['Authorization']
+    localStorage.removeItem('token')
+    commit('deleteUser')
   },
 }
 
@@ -30,6 +43,11 @@ const mutations = {
   setUser(state, user) {
     state.user = user
     state.is_auth = true
+  },
+
+  deleteUser(state) {
+    state.user = {}
+    state.is_auth = false
   },
 }
 
